9ea56b7fa35e0684a800eaf4b663c74b8180ab58
Juliano Cervelin <juliano.cervelin@gmail.com>
2024-11-08 20:16:34 +0000

    Fix test case, that calls openAI api

--- src/test/kotlin/io/jcervelin/services/OpenAIClientTest.kt
+++ src/test/kotlin/io/jcervelin/services/OpenAIClientTest.kt
@@ -1,62 +1,39 @@
 package io.jcervelin.services
 
-import io.jcervelin.j
 import io.jcervelin.models.History
 import io.jcervelin.plugins.configureClientSerialization
-import io.jcervelin.plugins.configureSerialization
+import io.jcervelin.toJson
 import io.ktor.client.*
 import io.ktor.client.engine.mock.*
 import io.ktor.http.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
-import io.ktor.server.testing.*
 import io.ktor.utils.io.*
 import kotlinx.coroutines.runBlocking
-import kotlin.test.Ignore
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 
 class OpenAIClientTest {
 
-    @Ignore
-    fun test()  {
+    @Test
+    fun `Should call openai api and return modified message`() {
         runBlocking {
-            val mockEngine = MockEngine { request ->
+            val openAIChatResponse =
+                OpenAIChatResponse(listOf(OpenAIChatChoice(OpenAIChatMessage("", "Something rude"))))
+            val mockEngine = MockEngine {
                 respond(
-                    content = ByteReadChannel("""
-                        
-                    """.trimIndent()),
+                    content = ByteReadChannel(openAIChatResponse.toJson()),
                     status = HttpStatusCode.OK,
                     headers = headersOf(HttpHeaders.ContentType, "application/json")
                 )
             }
+            val httpClient = HttpClient(mockEngine).config {
+                configureClientSerialization()
+            }
 
-
-            val openAIClient = OpenAIClient("apiKey", History(LinkedHashMap()), HttpClient(mockEngine))
+            val openAIClient = OpenAIClient("apiKey", History(LinkedHashMap()), httpClient)
             val rudeResponse = openAIClient.getRudeResponse("Something nice")
-            rudeResponse
 
+            assertEquals("Something rude", rudeResponse)
         }
-
-
-
-
-        val openAIChatResponse = OpenAIChatResponse(
-            listOf(
-                OpenAIChatChoice(
-                    OpenAIChatMessage(
-                        "someRole",
-                        "some rude message"
-                    )
-                )
-            )
-        )
-
-
-//        val rudeResponse = OpenAIClient("someApiKey", History(LRUCache(1)), client)
-//            .getRudeResponse("Something nice")
-
-//        assertEquals("some rude message", rudeResponse)
     }
 }
